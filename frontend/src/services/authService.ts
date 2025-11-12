e8c86ff6cce7e78d8cc22e86108dca65ed924f42
haszKEJL <haszKEJL@gmail.com>
2025-11-12 20:46:04 +0100

    asd

--- frontend/src/services/authService.ts
+++ frontend/src/services/authService.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-//const API_BASE_URL = 'https://projekt-pai-gr5.onrender.com/api';
-const API_BASE_URL = 'http://localhost:8000/api';
+const API_BASE_URL = 'https://projekt-pai-gr5.onrender.com/api';
+//const API_BASE_URL = 'http://localhost:8000/api';
 
 interface RegisterData {
   username: string;
